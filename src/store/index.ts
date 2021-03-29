529ade3a35fadba0c60ae08c34bb697e6aad4670
Tadpole <istack@yeah.net>
2021-03-29 13:26:51 +0800

    Revert "*"
    This reverts commit 51177fc9defcd6ea1cc03f4b253774b96914c2b2.

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,7 +13,7 @@ export default createStore({
   },
   plugins: [
     createPersistedState({
-      key: "vite-app-ts-cache",
+      key: "vue3-app-ts-cache",
       storage: {
         getItem: (key) => ls.get(key),
         setItem: (key, value) => ls.set(key, value),
